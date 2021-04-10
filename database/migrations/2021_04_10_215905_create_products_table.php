f60f181f742bcf4d76a575ecd96e1cabf813e3df
FadyShakerShawky <fadyshakershawky@gmail.com>
2021-04-11 00:28:49 +0200

    Simple test laravel api

---
+++ database/migrations/2021_04_10_215905_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("name");
+            $table->decimal("price",$precision = 8, $scale = 2);
+            $table->decimal("dicount",$precision = 8, $scale = 2);
+            $table->unsignedTinyInteger("quantity");
+            $table->string("img");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
